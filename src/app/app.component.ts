e177f58a316c938de87e47b3a013cd7f5640e0ed
ecuaryan <ryanwillmore@gmail.com>
2018-02-10 15:30:27 -0700

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -288,22 +288,27 @@ export class AppComponent {
 
   tiRadLevelOutcome = {
     'TR1' : {
+      'description': 'TR1',
       'recommendation': 'Benign',
       'notes': ['No FNA']
     },
     'TR2' : {
+      'description': 'TR2',
       'recommendation': 'Not Suspicious',
       'notes': ['No FNA']
     },
     'TR3' : {
+      'description': 'TR3',
       'recommendation': 'Mildly Suspicious',
       'notes': ['FNA if ≥ 2.5 cm', 'Follow if ≥ 1.5 cm']
     },
     'TR4' : {
+      'description': 'TR4',
       'recommendation': 'Moderately Suspicious',
       'notes': ['FNA if ≥ 1.5 cm', 'Follow if ≥ 1 cm']
     },
     'TR5' : {
+      'description': 'TR5',
       'recommendation': 'Highly Suspicious',
       'notes': ['FNA if ≥ 1 cm', 'Follow if ≥ 0.5 cm*', '*Refer to discussion of papillary microcarcinomas for 5-9 mm TR5 nodules.']
     }
